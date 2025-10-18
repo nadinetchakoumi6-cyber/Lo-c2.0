d91c17e333cc17a25c8fe55e8a26e6b89cff6b65
nadinetchakoumi6-cyber <nadinetchakoumi6@gmail.com>
2025-10-18 16:29:50 +0100

    Index.js
    const botDescription = "🎮 Bot Jeux Fun ! Jeux : Quiz, Devine le nombre, Pierre-papier-ciseaux, ⚽ eFootball 2025, 🥷 Naruto Storm. Écris le nom du jeu pour jouer et défie tes amis ! 😎";

---
+++ Index.js
@@ -0,0 +1,1 @@
+const botDescription = "🎮 Bot Jeux Fun ! Jeux : Quiz, Devine le nombre, Pierre-papier-ciseaux, ⚽ eFootball 2025, 🥷 Naruto Storm. Écris le nom du jeu pour jouer et défie tes amis ! 😎";
